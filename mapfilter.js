0dbfbabdc4e7cd9000ef316afc67237308c32538
priyanshu goel <priyanshugoel001@gmail.com>
2021-06-20 00:28:11 +0530

    react

---
+++ mapfilter.js
@@ -0,0 +1,10 @@
+let arr=[2,4,6,8,10];
+// traditional way
+for(let i=0;i<arr.length;i++)
+{
+    arr[i]*=2;
+}
+console.log(arr);
+
+//map: higher order function either take a function or return function
+
